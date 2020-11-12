78f0d7a54c37eb5eefa79df27acf0cdf92aaf35b
Major Hayden <major@mhtx.net>
2020-11-12 10:06:25 -0600

    Check formatting recursively

--- state-and-locks/terraform.tf
+++ state-and-locks/terraform.tf
@@ -1,50 +1,50 @@
 # AWS-specific configurations that are used throughout the deployment.
 provider "aws" {
-    region = "us-east-1"
-    profile = "imagebuilder"
-    shared_credentials_file = "~/.aws/credentials"
+  region                  = "us-east-1"
+  profile                 = "imagebuilder"
+  shared_credentials_file = "~/.aws/credentials"
 }
 
 resource "aws_s3_bucket" "terraform-state" {
-    bucket = "imagebuilder-terraform-state"
+  bucket = "imagebuilder-terraform-state"
 
-    versioning {
-        enabled = true
-    }
+  versioning {
+    enabled = true
+  }
 
-    lifecycle {
-        prevent_destroy = true
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 
-    # Enable server-side encryption by default
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
+  }
 
-    tags = {
-        Name = "Image Builder Terraform State Storage"
-        ServiceOwner = "Image Builder"
-        AppCode = "IMGB-001"
-    }
+  tags = {
+    Name         = "Image Builder Terraform State Storage"
+    ServiceOwner = "Image Builder"
+    AppCode      = "IMGB-001"
+  }
 }
 
 resource "aws_dynamodb_table" "terraform-locks" {
-    name = "imagebuilder-terraform-locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+  name         = "imagebuilder-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-    tags = {
-        Name = "Image Builder Terraform Locking"
-        ServiceOwner = "Image Builder"
-        AppCode = "IMGB-001"
-    }
+  tags = {
+    Name         = "Image Builder Terraform Locking"
+    ServiceOwner = "Image Builder"
+    AppCode      = "IMGB-001"
+  }
 }
